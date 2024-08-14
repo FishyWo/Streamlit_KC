e90664de31380adef27ce77f71e097cd5ffcb04b
Guanyu <66564308+FishyWo@users.noreply.github.com>
2024-08-14 16:14:28 +0800

    Add files via upload

--- Streamlit_KC.py
+++ Streamlit_KC.py
@@ -8,16 +8,17 @@ model = joblib.load('./models/GBR_KCHouseSalesMk1.pkl')
 #Initialising session state variables
 if 'predicted_value' not in st.session_state:
     st.session_state.predicted_value = 0
-min_lat = [47.6256, 47.1593, 47.5380, 47.3942, 47.2574, 47.6480, 47.1559, 47.4824, 47.6394, 47.2558, 47.4142, 47.4545]
-max_lat = [47.7776, 47.4536, 47.6760, 47.5935, 47.4396, 47.7769, 47.7654, 47.6515, 47.7759, 47.4143, 47.5473, 47.6609]
-min_long = [-122.416, -122.113, -122.225, -122.515, -122.519 , -122.286, -121.924, -122.327, -122.124, -122.243 , -122.283, -122.094]
-max_long = [-122.263, -121.867, -122.088, -122.277, -122.244, -122.118, -121.315, -122.211, -121.857, -122.105, -122.063, -121.892]
+min_lat = [47.4816, 47.3942, 47.1593, 47.6021, 47.2574, 47.1559, 47.3866, 47.612]
+max_lat = [47.7754, 47.6118, 47.4812, 47.7769, 47.4427, 47.7654, 47.6025, 47.7776]
+min_long = [-122.134, -122.515, -122.174, -122.262, -122.519, -121.943, -122.267, -122.416]
+max_long = [-121.868, -122.241, -121.867, -122.048, -122.174, -121.315, -122.035, -122.256]
 bathroom_vals = []
 
 class Features:
-    def __init__(self, bedrooms, bathrooms, sqft_lot, floors, waterfront, view, condition, grade, sqft_above, sqft_basement, lat, long, sqft_living15, sqft_lot15, no_yrs_built, no_yrs_renovated, yr_sold, neighbourhood_0=0, neighbourhood_1=0, neighbourhood_2=0,neighbourhood_3=0,neighbourhood_4=0,neighbourhood_5=0,neighbourhood_6=0,neighbourhood_7=0,neighbourhood_8=0,neighbourhood_9=0,neighbourhood_10=0,neighbourhood_11=0):
+    def __init__(self, bedrooms, bathrooms, sqft_living, sqft_lot, floors, waterfront, view, condition, grade, sqft_above, sqft_basement, lat, long, sqft_living15, sqft_lot15, no_yrs_built, no_yrs_renovated, yr_sold, neighbourhood_0=0, neighbourhood_1=0, neighbourhood_2=0,neighbourhood_3=0,neighbourhood_4=0,neighbourhood_5=0,neighbourhood_6=0,neighbourhood_7=0):
         self.bedrooms = bedrooms
         self.bathrooms = bathrooms
+        self.sqft_living = sqft_living
         self.sqft_lot= sqft_lot
         self.floors= floors
         self.waterfront = waterfront
@@ -41,16 +42,17 @@ class Features:
         self.neighbourhood_5 = neighbourhood_5
         self.neighbourhood_6 = neighbourhood_6
         self.neighbourhood_7 = neighbourhood_7
-        self.neighbourhood_8 = neighbourhood_8
-        self.neighbourhood_9 = neighbourhood_9
-        self.neighbourhood_10 = neighbourhood_10
-        self.neighbourhood_11 = neighbourhood_11
+        # self.neighbourhood_8 = neighbourhood_8
+        # self.neighbourhood_9 = neighbourhood_9
+        # self.neighbourhood_10 = neighbourhood_10
+        # self.neighbourhood_11 = neighbourhood_11
 
     def return_object(self):
         # Convert the features to a dictionary
         return {
             'bedrooms': self.bedrooms,
             'bathrooms': self.bathrooms,
+            'sqft_living': self.sqft_living,
             'sqft_lot': self.sqft_lot,
             'floors': self.floors,
             'waterfront': self.waterfront,
@@ -74,15 +76,11 @@ class Features:
             'neighbourhood_5': self.neighbourhood_5,
             'neighbourhood_6': self.neighbourhood_6,
             'neighbourhood_7': self.neighbourhood_7,
-            'neighbourhood_8': self.neighbourhood_8,
-            'neighbourhood_9': self.neighbourhood_9,
-            'neighbourhood_10': self.neighbourhood_10,
-            'neighbourhood_11': self.neighbourhood_11
         }
 def predictModel():
     global predicted_value
     #Sum up all the bathrooms
-    feature_input = Features(bedrooms, sum(bathroom_vals), sqft_lot, floors, int(waterfront), view, condition, grade, sqft_above, sqft_basement, lat, long, sqft_living15, sqft_lot15, no_yrs_built, no_yrs_renovated, yr_sold)
+    feature_input = Features(bedrooms, sum(bathroom_vals), (sqft_above+sqft_basement), sqft_lot, floors, int(waterfront), view, condition, grade, sqft_above, sqft_basement, lat, long, sqft_living15, sqft_lot15, no_yrs_built, no_yrs_renovated, yr_sold)
     setattr(feature_input, f'neighbourhood_{int(neighbourhood)-1}', 1)
     feature_input = pd.DataFrame(feature_input.return_object(), index=[0])
     print(feature_input)
@@ -99,7 +97,7 @@ with col1:
     view = st.slider(label='view', min_value=0, max_value=4)
     condition = st.slider(label='Condition of the apartment', min_value=1, max_value=5)
     grade = st.slider(label='Grade of Construction and Design', min_value=1, max_value=13)
-    neighbourhood = st.selectbox(label='Which neighbourhood', options=[i+1 for i in range(12)])
+    neighbourhood = st.selectbox(label='Which neighbourhood', options=[i+1 for i in range(8)])
     lat = st.number_input(label='lat', min_value=min_lat[neighbourhood-1],max_value=max_lat[neighbourhood-1], step=0.0001, format='%.4f')
     long = st.number_input(label='long', min_value=min_long[neighbourhood-1],max_value=max_long[neighbourhood-1], step=0.001, format='%.3f')
 
@@ -133,4 +131,4 @@ with bathrooms_cont:
 
 with col3:
     st.write(st.session_state.predicted_value)
-    st.button(label='Predict', on_click=predictModel)
+    st.button(label='Predict', on_click=predictModel())
